cd750fb684c36a0ac58e5504639e2724ba0bf893
Jack Chen <23hchen@athenian.org>
2022-02-16 19:24:50 -0800

    Finished 2/16/2022

--- src/main/java/AnimalChooser.java
+++ src/main/java/AnimalChooser.java
@@ -23,9 +23,38 @@ public class AnimalChooser {
         JButton button1 = new JButton("Red Panda");
         frame.add(button1, BorderLayout.SOUTH);
 
+        JButton button2 = new JButton("Seal");
+        frame.add(button2, BorderLayout.WEST);
+
+        JButton button3 = new JButton("Kangaroo");
+        frame.add(button3, BorderLayout.EAST);
+
+        JButton button4 = new JButton("Giraffe");
+        frame.add(button4, BorderLayout.NORTH);
+
+
+
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 ImageIcon icon = new ImageIcon("images/red_panda.jpg");
+                label1.setIcon(icon);
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ImageIcon icon = new ImageIcon("images/seal.jpg");
+                label1.setIcon(icon);
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ImageIcon icon = new ImageIcon("images/Roger-the-musclebound-kangaroo-is-back--and-it-looks-like-hes-been-hitting-the-weights.jpg");
+                label1.setIcon(icon);
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ImageIcon icon = new ImageIcon("images/6b3686268192f8385f22e080da1669c0--fat-animals-funny-animals.jpg");
                 label1.setIcon(icon);
             }
         });
--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -9,6 +9,50 @@ public class TemperatureConverter {
 
     public TemperatureConverter() {
         // YOUR CODE HERE!
+        // 1. Create the frame (the window that will pop up)
+        JFrame frame = new JFrame("Temperature Converter");
+        frame.setLayout(new FlowLayout());
+
+        // 2. Choose what happens when you click the exit button
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 3. Create components and put them in the frame
+        JLabel label1 = new JLabel("F");
+        frame.add(label1);
+
+        JTextField field1 = new JTextField(14);
+        frame.add(field1);
+
+        JButton button1 = new JButton("Convert to C");
+        frame.add(button1);
+
+        JLabel label2 = new JLabel("C");
+        frame.add(label2);
+
+        JTextField field2 = new JTextField(14);
+        frame.add(field2);
+
+        JButton button2 = new JButton("Convert to F");
+        frame.add(button2);
+
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                field2.setText(((Double.parseDouble(field1.getText()) - 32)*(5/9)) + "");
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                field1.setText(((Double.parseDouble(field2.getText())*(5/9) + 32)) + "");
+            }
+        });
+
+
+        // 4. Size the frame
+        frame.setSize(300, 300);
+
+        // 5. Show the frame
+        frame.setVisible(true);
     }
 
     public static void main(String args[]){
